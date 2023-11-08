683a5cf1e248334fa1bfc7177b69901f22ff56d5
thomcm <tmaltby19@gmail.com>
2023-11-08 20:52:17 +0700

    redirect homepage to lils

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
 'use client'
+import useHref from '@/utils/services/useHref'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
 export default function Page() {
+    const { lilsLink } = useHref()
+
+    const router = useRouter()
+
+    useEffect(() => {
+        router.push(lilsLink)
+    }, [])
+
     return <div></div>
 }
