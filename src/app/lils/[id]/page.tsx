cfafe81fe5d8ba7d3f65e1dfef55c7eec36b1751
Tom <tmaltby19@gmail.com>
2024-04-23 00:11:16 +0700

    add og data

--- src/app/lils/[id]/page.tsx
+++ src/app/lils/[id]/page.tsx
@@ -1,7 +1,28 @@
-'use client'
-
 import NounPage from '@/components/NounPage/NounPage'
+import type { Metadata } from 'next'
+
+type PageProps = {
+    params: { id: number }
+}
 
 export default function Page({ params }: { params: { id: number } }) {
     return <NounPage project="LilNouns" nounId={params.id} />
+}
+
+export async function generateMetadata({
+    params,
+}: PageProps): Promise<Metadata> {
+    const id = params.id
+
+    const title = `Lil Noun ${id} | Probe`
+    const description = `Date of birth, colors and stats of Lil Noun ${id}.`
+
+    return {
+        title: title,
+        description: description,
+        openGraph: {
+            title: title,
+            description: description,
+        },
+    }
 }
