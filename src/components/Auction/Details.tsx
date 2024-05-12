993a97e76db18acc336f7ce70ad75f1fc3ccff2a
Tom <tmaltby19@gmail.com>
2024-05-12 22:28:10 +0700

    init

--- src/components/Auction/Details.tsx
+++ src/components/Auction/Details.tsx
@@ -4,24 +4,74 @@ import {
     useWeb3ModalProvider,
     useWeb3ModalAccount,
 } from '@web3modal/ethers/react'
-import { BrowserProvider, Contract } from 'ethers'
+import { formatEther, BrowserProvider, Contract } from 'ethers'
+import { useMemo, useState } from 'react'
 
-// Nouns Contract Address and ABI
 const nounsContractAddress = process.env
-    .NEXT_PUBLIC_NOUNS_CONTRACT_ADDRESS_PROXY as string
+    .NEXT_PUBLIC_NOUNS_CONTRACT_ADDRESS as string
 
 const nounsContractABI = [
-    // Include the entire ABI provided above
-    'function auction() view returns (uint256, uint256, uint256, uint256, address payable, bool)',
-    'function createBid(uint256 nounId) payable',
-    'function createBidWithComment(uint256 nounId, string comment) payable',
-    // More functions as needed...
+    // Read the current auction state
+    'function auction() public view returns (uint256 nounId, uint128 amount, uint256 startTime, uint256 endTime, address bidder, bool settled)',
+
+    // Function to place a bid on the current auction
+    'function createBid(uint256 nounId) external payable',
+
+    // Function to place a bid with a comment
+    'function createBidWithComment(uint256 nounId, string comment) external payable',
+
+    // Function to retrieve the duration of auctions
+    'function duration() public view returns (uint256)',
+
+    // Function to retrieve the minimum percentage difference between bids
+    'function minBidIncrementPercentage() public view returns (uint8)',
+
+    // Address of the Nouns ERC721 token contract
+    'function nouns() public view returns (address)',
+
+    // Address of the contract owner
+    'function owner() public view returns (address)',
+
+    // Check if the contract is paused
+    'function paused() public view returns (bool)',
+
+    // Minimum price accepted in an auction
+    'function reservePrice() public view returns (uint192)',
+
+    // Minimum time left in an auction after a new bid
+    'function timeBuffer() public view returns (uint56)',
+
+    // Address of the WETH contract
+    'function weth() public view returns (address)',
 ]
 
-function NounsComponent() {
+function AuctionDetails() {
     const { address, isConnected } = useWeb3ModalAccount()
 
     const { walletProvider } = useWeb3ModalProvider()
+
+    const [auctionDetails, setAuctionDetails] = useState<{
+        nounId: number
+        amount: string
+        startTime: number
+        endTime: number
+        bidder: string
+        settled: boolean
+    }>()
+
+    const auctionStartTimeAsDate = useMemo(() => {
+        if (!auctionDetails) return null
+
+        return new Date(auctionDetails.startTime * 1000)
+    }, [auctionDetails?.startTime])
+
+    const auctionEndTimeAsDate = useMemo(() => {
+        if (!auctionDetails) return null
+
+        return new Date(auctionDetails.startTime * 1000)
+    }, [auctionDetails?.startTime])
+
+    const [auctionDuration, setAuctionDuration] = useState<number>()
 
     async function getAuctionDetails() {
         if (!isConnected) throw Error('User disconnected')
@@ -38,8 +88,50 @@ function NounsComponent() {
             signer
         )
 
-        const auctionDetails = await nounsContract.auction()
-        console.log(auctionDetails)
+        // const auctionDetails = await nounsContract.auction()
+        // console.log('auctionDetails', auctionDetails)
+        //         console.log('auctionDetails.result', auctionDetails.result)
+
+        const { nounId, amount, startTime, endTime, bidder, settled } =
+            await nounsContract.auction()
+
+        setAuctionDetails({
+            nounId: Number(nounId),
+            amount: formatEther(amount),
+            startTime: Number(startTime),
+            endTime: Number(endTime),
+            bidder,
+            settled,
+        })
+
+        console.log('nounId:', nounId.toString())
+        console.log('amount:', formatEther(amount))
+        console.log('startTime:', new Date(Number(startTime) * 1000))
+        console.log('endTime:', new Date(Number(endTime) * 1000))
+        console.log('bidder:', bidder)
+        console.log('settled:', settled)
+
+        getAuctionDuration()
+    }
+
+    async function getAuctionDuration() {
+        if (!isConnected) throw Error('User disconnected')
+
+        if (!walletProvider) throw Error('Wallet provider not found')
+
+        const ethersProvider = new BrowserProvider(walletProvider)
+
+        const signer = await ethersProvider.getSigner()
+
+        const nounsContract = new Contract(
+            nounsContractAddress,
+            nounsContractABI,
+            signer
+        )
+
+        const duration = await nounsContract.duration()
+
+        setAuctionDuration(Number(duration))
     }
 
     async function placeBid(nounId: number, amount: string) {
@@ -64,20 +156,75 @@ function NounsComponent() {
     }
 
     return (
-        <div className="space-y-3">
-            <div>
+        <div className="space-y-6">
+            <div className="space-y-3">
                 <button onClick={() => getAuctionDetails()}>
                     Get Auction Details
                 </button>
+
+                {auctionDetails && (
+                    <ul>
+                        <li>
+                            <strong>Noun ID:</strong> {auctionDetails.nounId}
+                        </li>
+
+                        <li>
+                            <strong>Amount:</strong> {auctionDetails.amount}
+                        </li>
+
+                        <li>
+                            <strong>Start Time:</strong>{' '}
+                            {auctionDetails.startTime.toString()}
+                        </li>
+
+                        {auctionStartTimeAsDate && (
+                            <li>
+                                <strong>Start Time (as date):</strong>{' '}
+                                {auctionStartTimeAsDate.toString()}
+                            </li>
+                        )}
+
+                        <li>
+                            <strong>End Time:</strong>{' '}
+                            {auctionDetails.endTime.toString()}
+                        </li>
+
+                        {auctionEndTimeAsDate && (
+                            <li>
+                                <strong>End Time (as date):</strong>{' '}
+                                {auctionEndTimeAsDate.toString()}
+                            </li>
+                        )}
+
+                        <li>
+                            <strong>Bidder:</strong> {auctionDetails.bidder}
+                        </li>
+
+                        <li>
+                            <strong>Settled:</strong>{' '}
+                            {auctionDetails.settled.toString()}
+                        </li>
+
+                        {auctionDuration && (
+                            <li>
+                                <strong>Duration:</strong> {auctionDuration}
+                            </li>
+                        )}
+                    </ul>
+                )}
             </div>
 
-            <div>
-                <button onClick={() => placeBid(1, '1000000000000000000')}>
-                    Place Bid on Noun #1
-                </button>
-            </div>
+            {auctionDetails && (
+                <div>
+                    <button
+                        onClick={() => placeBid(auctionDetails.nounId, '1')}
+                    >
+                        Place Bid on Noun #{auctionDetails.nounId}
+                    </button>
+                </div>
+            )}
         </div>
     )
 }
 
-export default NounsComponent
+export default AuctionDetails
--- src/utils/contexts/Web3ModalContext.ts
+++ src/utils/contexts/Web3ModalContext.ts
@@ -6,6 +6,9 @@ import React from 'react'
 // 1. Get projectId at https://cloud.walletconnect.com
 const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID as string
 const infuraApiKey = process.env.NEXT_PUBLIC_INFURA_API_KEY as string
+const defaultChainId = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID as string)
+const mainnetRpcUrl = `https://mainnet.infura.io/v3/${infuraApiKey}`
+const sepoliaRpcUrl = `https://sepolia.infura.io/v3/${infuraApiKey}`
 
 // 2. Set chains
 const mainnet = {
@@ -13,7 +16,7 @@ const mainnet = {
     name: 'Ethereum',
     currency: 'ETH',
     explorerUrl: 'https://etherscan.io',
-    rpcUrl: 'https://cloudflare-eth.com',
+    rpcUrl: mainnetRpcUrl,
 }
 
 const sepolia = {
@@ -21,7 +24,7 @@ const sepolia = {
     name: 'Sepolia',
     currency: 'ETH',
     explorerUrl: 'https://sepolia.etherscan.io',
-    rpcUrl: `https://sepolia.infura.io/v3/${infuraApiKey}`,
+    rpcUrl: sepoliaRpcUrl,
 }
 
 // 3. Create a metadata object
@@ -41,17 +44,15 @@ const ethersConfig = defaultConfig({
     enableEIP6963: true, // true by default
     enableInjected: true, // true by default
     enableCoinbase: true, // true by default
-    rpcUrl: '...', // used for the Coinbase SDK
-    defaultChainId: 1, // used for the Coinbase SDK
+    rpcUrl: defaultChainId == 1 ? mainnetRpcUrl : sepoliaRpcUrl, // used for the Coinbase SDK
+    defaultChainId: defaultChainId, // used for the Coinbase SDK
 })
 
 // 5. Create a Web3Modal instance
 createWeb3Modal({
     ethersConfig,
     chains: [mainnet, sepolia].filter(
-        (chain) =>
-            chain.chainId ==
-            parseInt(process.env.NEXT_PUBLIC_CHAIN_ID as string)
+        (chain) => chain.chainId == defaultChainId
     ),
     projectId,
     enableAnalytics: true, // Optional - defaults to your Cloud configuration
