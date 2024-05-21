7655c87deb4435e853e731bdb6892f276263780e
Tom <tmaltby19@gmail.com>
2024-05-21 20:50:02 +0700

    fix use client issue

--- src/components/Provider/Auction.tsx
+++ src/components/Provider/Auction.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React, { useContext, useEffect, useState } from 'react'
 import AuctionContext from '@/utils/contexts/AuctionContext'
 import RpcContext from '@/utils/contexts/RpcContext'
