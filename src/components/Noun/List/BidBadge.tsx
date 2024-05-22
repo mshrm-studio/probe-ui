f5cc7751403881ecd645f31c9beea1838de188e0
Tom <tmaltby19@gmail.com>
2024-05-22 17:21:41 +0700

    ui updates

--- src/components/Noun/List/BidBadge.tsx
+++ src/components/Noun/List/BidBadge.tsx
@@ -11,7 +11,11 @@ const NounListBidBage: React.FC<Props> = ({ nounId }) => {
 
     const auctionActive = useAuctionStatus(auction)
 
+    if (!auction) return null
+
     if (!auctionActive) return null
+
+    if (auction.nounId !== nounId) return null
 
     return (
         <div className="bg-[#FFEF2E] font-bold px-2 py-0.5 uppercase absolute right-0 bottom-0 text-[13px] translate-x-1 translate-y-1">
