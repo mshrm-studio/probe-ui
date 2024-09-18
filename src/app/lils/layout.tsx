6d7b965226541c203785af7174da63d1c747e93b
Tom <tmaltby19@gmail.com>
2024-09-18 14:59:46 +0700

    remove auction provider for lils, contract address incorrect

--- src/app/lils/layout.tsx
+++ src/app/lils/layout.tsx
@@ -1,6 +1,5 @@
 import AuctionHouseContractProvider from '@/components/Provider/AuctionHouseContract'
 import TokenContractProvider from '@/components/Provider/TokenContract'
-import AuctionProvider from '@/components/Provider/Auction'
 
 type Props = {
     children: React.ReactNode
@@ -10,7 +9,7 @@ export default function LilsLayout({ children }: Props) {
     return (
         <AuctionHouseContractProvider project="LilNouns">
             <TokenContractProvider project="LilNouns">
-                <AuctionProvider>{children}</AuctionProvider>
+                {children}
             </TokenContractProvider>
         </AuctionHouseContractProvider>
     )
